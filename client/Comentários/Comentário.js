0d825197062081f42ba158d0a23ad4f9bcb678a3
Amanda Batista <dsbamanda@gmail.com>
2018-08-03 23:56:30 +0000

    Criar página especifica para o Perfil

--- client/Comentários/Comentário.js
+++ client/Comentários/Comentário.js
@@ -1,7 +1,7 @@
 Template.Comentario.helpers({
-   usernameDoAutor: function () {
-       var idDoAutor = this.autor;
-       var autor = Meteor.users.findOne({_id: idDoAutor});
-       return autor.username;
-   }
+ usernameDoAutor: function () {
+   var idDoAutor = this.autor;
+   var autor = Meteor.users.findOne({_id: idDoAutor});
+   return autor.username;
+ }
 });
